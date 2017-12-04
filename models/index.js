a7086a0b588d069cd547e1e8759a28eab0f50b7b
Johann <schuster.bass@gmail.com>
2017-12-04 18:50:09 +0000

    db

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.set('debug', true)
-mongoose.connect('mongodb://localhost/questions-api')
-
+mongoose.connect('mongodb://johann:123bloemfontein@ds129796.mlab.com:29796/js-questions-api')
+// mongoose.connect('mongodb://localhost/questions-api')
+// mongodb://<dbuser>:<dbpassword>@ds129796.mlab.com:29796/js-questions-api
 mongoose.Promise = Promise 
 
 module.exports.Question = require('./question')
