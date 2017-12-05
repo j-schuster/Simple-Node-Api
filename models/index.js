364e2a33703d89b402e391370fb8dd3b36459594
Johann <schuster.bass@gmail.com>
2017-12-05 16:01:16 +0000

    gitignore

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-//const passwords = require('../passwords')
+const passwords = require('../passwords')
 mongoose.set('debug', true)
 
 
-mongoose.connect('mongodb://johann:123bloemfontein@ds129796.mlab.com:29796/js-questions-api')
+mongoose.connect(passwords.DATABASE)
 // mongoose.connect('mongodb://localhost/questions-api')
 
 mongoose.Promise = Promise 
