62ace75bbb18fe46c7d13d383d6cb3d58e00a4c5
Johann <schuster.bass@gmail.com>
2017-12-05 15:58:56 +0000

    db fix

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const passwords = require('../passwords')
 mongoose.set('debug', true)
 
 
-mongoose.connect(passwords.DATABASE)
+mongoose.connect('mongodb://johann:123bloemfontein@ds129796.mlab.com:29796/js-questions-api')
 // mongoose.connect('mongodb://localhost/questions-api')
 
 mongoose.Promise = Promise 
